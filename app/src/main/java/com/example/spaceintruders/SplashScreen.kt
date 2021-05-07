77ba25a0c370f6a571b69da20de13ad8b824ad3f
Cam <cta67@uclive.ac.nz>
2021-05-07 17:49:20 +1200

    splash screen code!?

---
+++ app/src/main/java/com/example/spaceintruders/SplashScreen.kt
@@ -0,0 +1,23 @@
+package com.example.spaceintruders
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import android.content.Intent
+import android.os.Handler
+import android.view.WindowManager
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+
+
+        Handler().postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }, 3000) //milliseconds.
+    }
+}
