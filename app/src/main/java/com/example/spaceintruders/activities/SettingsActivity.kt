2774bbd4f12aa6ab26da8b7564fda82b8c48c41c
Cam <cta67@uclive.ac.nz>
2021-05-25 15:58:31 +1200

    Starting to add shared preferences saving for settings, but progress heavily limited by an android studio version issue with gradle right now, so not much can really be done

--- app/src/main/java/com/example/spaceintruders/activities/SettingsActivity.kt
+++ app/src/main/java/com/example/spaceintruders/activities/SettingsActivity.kt
@@ -1,11 +1,15 @@
 package com.example.spaceintruders.activities
 
+import android.content.Context
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
 import com.example.spaceintruders.R
 
 class SettingsActivity : AppCompatActivity() {
+
+    var username = ""
+    var colour = ""
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,6 +21,21 @@ class SettingsActivity : AppCompatActivity() {
                 .commit()
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
+
+    fun saveSettings(){
+        val sharedPref = activity?.getSharedPreferences("profile_settings", Context.MODE_PRIVATE)?: return
+        with(sharedPref.edit()) {
+            putString("name", username)
+            putString("colour", colour)
+            commit()
+        }
+    }
+
+    fun loadSettings(){
+        val sharedPref = activity?.getSharedPreferences("profile_settings", Context.MODE_PRIVATE)?: return
+        username = sharedPref.getString("name", "username")
+        colour = sharedPref.getString("colour", "white")
     }
 
     class SettingsFragment : PreferenceFragmentCompat() {
