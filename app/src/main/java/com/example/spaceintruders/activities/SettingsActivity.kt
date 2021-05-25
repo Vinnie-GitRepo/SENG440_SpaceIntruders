a5d842d9a928df02821a9be936ad3d2445e57a0a
Cam <cta67@uclive.ac.nz>
2021-05-25 18:14:40 +1200

    Creating settings layout page, adding colour wheel, and successfully hooking up saving and loading settings with shared preferences

--- app/src/main/java/com/example/spaceintruders/activities/SettingsActivity.kt
+++ app/src/main/java/com/example/spaceintruders/activities/SettingsActivity.kt
@@ -1,27 +1,50 @@
 package com.example.spaceintruders.activities
 
 import android.content.Context
+import android.graphics.Color
 import android.os.Bundle
+import android.text.TextWatcher
 import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
+import com.apandroid.colorwheel.ColorWheel
 import com.example.spaceintruders.R
 
 class SettingsActivity : AppCompatActivity() {
 
     var username: String =  ""
-    var colour: String = "#FFFFFF"
+    var colour: Int = Color.rgb(255, 255, 255)
+    var darkmode: Boolean = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.settings_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager
-                .beginTransaction()
-                .replace(R.id.settings, SettingsFragment())
-                .commit()
-        }
+//        if (savedInstanceState == null) {
+//            supportFragmentManager
+//                .beginTransaction()
+//                .replace(R.id.settings, SettingsFragment())
+//                .commit()
+//        }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        var usernameField = findViewById<EditText>(R.id.usernameinputtext)
+        var darkswitch = findViewById<Switch>(R.id.darkmodeswitch)
+        var colourWheel = findViewById<ColorWheel>(R.id.colorWheel)
+        var saveSettingButton = findViewById<Button>(R.id.savesettingsbutton)
+
+        loadSettings(findViewById(android.R.id.content))
+        usernameField.setText(username)
+        darkswitch.isChecked = darkmode
+        wheel()
+
+        saveSettingButton.setOnClickListener{
+            username = usernameField.text.toString()
+            darkmode = darkswitch.isChecked()
+            saveSettings(findViewById(android.R.id.content))
+        }
     }
 
 
@@ -29,7 +52,8 @@ class SettingsActivity : AppCompatActivity() {
         val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
         with(sharedPref.edit()) {
             putString("name", username)
-            putString("colour", colour)
+            putInt("colour", colour)
+            putBoolean("darkmode", darkmode)
             commit()
         }
     }
@@ -37,14 +61,23 @@ class SettingsActivity : AppCompatActivity() {
     fun loadSettings(view: View) {
         val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
         username = sharedPref.getString("name", "username").orEmpty()
-        colour = sharedPref.getString("colour", "#FFFFFF").orEmpty()
+        colour = sharedPref.getInt("colour", Color.rgb(255, 255, 255))
+        darkmode = sharedPref.getBoolean("darkmode", false)
 
     }
 
-
-    class SettingsFragment : PreferenceFragmentCompat() {
-        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
-            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+    fun wheel() {
+        var colourWheel = findViewById<ColorWheel>(R.id.colorWheel)
+        colourWheel.rgb = colour
+        colourWheel.colorChangeListener = { rgb: Int ->
+            colour = colourWheel.rgb
         }
     }
+
+
+//    class SettingsFragment : PreferenceFragmentCompat() {
+//        override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+//            setPreferencesFromResource(R.xml.root_preferences, rootKey)
+//        }
+//    }
 }
