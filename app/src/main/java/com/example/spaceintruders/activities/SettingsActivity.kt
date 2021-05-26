62445305dc4b840c2fb382446be62c570b80d3ca
Cam <cta67@uclive.ac.nz>
2021-05-26 18:15:18 +1200

    Ship image colouring using colour from wheel/preferences.

--- app/src/main/java/com/example/spaceintruders/activities/SettingsActivity.kt
+++ app/src/main/java/com/example/spaceintruders/activities/SettingsActivity.kt
@@ -2,11 +2,15 @@ package com.example.spaceintruders.activities
 
 import android.content.Context
 import android.graphics.Color
+import android.graphics.ColorFilter
+import android.graphics.PorterDuff
+import android.media.Image
 import android.os.Bundle
 import android.text.TextWatcher
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.ImageView
 import android.widget.Switch
 import androidx.appcompat.app.AppCompatActivity
 import androidx.preference.PreferenceFragmentCompat
@@ -28,16 +32,28 @@ class SettingsActivity : AppCompatActivity() {
         }
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        var colourWheel = findViewById<ColorWheel>(R.id.colorWheel)
-        var saveSettingButton = findViewById<Button>(R.id.savesettingsbutton)
-
         loadSettings(findViewById(android.R.id.content))
-        wheel()
 
-        saveSettingButton.setOnClickListener{
-            saveSettings(findViewById(android.R.id.content))
+        var shipImage = findViewById<ImageView>(R.id.shipImage)
+        shipImage.setColorFilter(colour, PorterDuff.Mode.SRC_ATOP)
+
+        var colourWheel = findViewById<ColorWheel>(R.id.colorWheel)
+        colourWheel.rgb = colour
+        colourWheel.colorChangeListener = { rgb: Int ->
+            colour = colourWheel.rgb
+            shipImage.setColorFilter(colour, PorterDuff.Mode.SRC_ATOP)
         }
 
+//        var saveSettingButton = findViewById<Button>(R.id.savesettingsbutton)
+//        saveSettingButton.setOnClickListener{
+//            saveSettings(findViewById(android.R.id.content))
+//        }
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+        saveSettings(findViewById(android.R.id.content))
     }
 
     fun saveSettings(view: View) {
@@ -52,16 +68,6 @@ class SettingsActivity : AppCompatActivity() {
         val sharedPref = this.getPreferences(Context.MODE_PRIVATE) ?: return
         colour = sharedPref.getInt("colour", Color.rgb(255, 255, 255))
     }
-
-    fun wheel() {
-        var colourWheel = findViewById<ColorWheel>(R.id.colorWheel)
-        colourWheel.rgb = colour
-        colourWheel.colorChangeListener = { rgb: Int ->
-            colour = colourWheel.rgb
-        }
-    }
-
-
 
 
 
