82b66945918718dce57c2b0f9d70001db2a8fc1b
Vinnie <vjj14@uclive.ac.nz>
2021-05-27 17:25:16 +1200

    Added a data class for recording game outcomes, which is being done in preparation for the room database implementation.
    The build file was also updated to include room dependencies.

---
+++ app/src/main/java/com/example/spaceintruders/data/GameRecord.kt
@@ -0,0 +1,13 @@
+package com.example.spaceintruders.data
+
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+/**
+ * Class for objects containing information about a previous game outcome.
+ * Stores the names of the players who played the game and their scores.
+ */
+@Entity
+class GameRecord {
+//    @PrimaryKey val id: Int
+}
